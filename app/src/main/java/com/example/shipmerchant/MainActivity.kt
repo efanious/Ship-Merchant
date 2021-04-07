06db8b684426bcbfda20554e7b52f5d1aef06ad8
Efana Usua <Efana.usua@wemabank.com>
2021-04-07 11:12:21 +0100

    testing calls

--- app/src/main/java/com/example/shipmerchant/MainActivity.kt
+++ app/src/main/java/com/example/shipmerchant/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.shipmerchant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //TODO set up
+        val quoteButton = findViewById(R.id.requestQuoteButton) as Button
+        quoteButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, QuoteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/shipmerchant/network/MerchantApiService.kt
+++ app/src/main/java/com/example/shipmerchant/network/MerchantApiService.kt
@@ -1,18 +1,31 @@
 package com.example.shipmerchant.network
 
+import com.example.shipmerchant.requests.QuoteRequest
+import com.example.shipmerchant.response.QuoteResponse
+import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 
 private const val BASE_URL = "https://staging-shopmax.herokuapp.com/api-docs/#/"
 
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(GsonConverterFactory.create())
     .baseUrl(BASE_URL)
     .build()
 
 interface MerchantApiService {
-    @GET("shipments/quotes")
-    fun getQuotes()
+    @POST("shipments/quotes")
+    fun getQuotes(@Body quoteRequest: QuoteRequest):
+            Call<QuoteResponse>
+}
+
+object MerchantApi {
+    val retrofitService : MerchantApiService by lazy {
+        retrofit.create(MerchantApiService::class.java)
+    }
 }
